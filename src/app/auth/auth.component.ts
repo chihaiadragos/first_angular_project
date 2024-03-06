af405b7390b6fcd34cdd47371ff47d5f73827b70
chihaia dragos <dragos.chihaia.gabriel@gmail.com>
2024-03-06 20:53:58 +0200

    dashboard, services

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,29 +1,33 @@
 import { Component } from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatCard, MatCardContent} from "@angular/material/card";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-    imports: [
-        MatButton,
-        MatCard,
-        MatCardContent,
-        MatFormField,
-        MatInput,
-        MatLabel,
-        ReactiveFormsModule
-    ],
+  imports: [
+    MatButton,
+    MatCard,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule,
+    NgIf,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
   authForm: FormGroup;
+  viewType: string = "login";
 
-  constructor(formBuilder:FormBuilder) {
+  constructor(formBuilder:FormBuilder, private authService: AuthService) {
     this.authForm = formBuilder.group({
       username:["", Validators.required],
       email:["", Validators.required],
@@ -32,12 +36,19 @@ export class AuthComponent {
     });
   }
   onSave(): void {
-    alert("Am dat save");
+    alert("Data saved");
     if (this.authForm.valid){
       console.log(this.authForm.value);
-      alert("Formularul este valid");
+      const body = this.authForm.value;
+      this.authService.register(body).subscribe((response: any) => {
+        console.log(response);
+      });
     } else {
-      alert("Formularul este invalid");
+      alert("Form is invalid");
     }
+  }
+  onSetViewType(viewType: string) {
+    console.log(viewType);
+    this.viewType = viewType;
   }
 }
